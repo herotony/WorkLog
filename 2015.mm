3f3e92530c8103fa49e99308651cbf94617ff5c1
herotony <herotony@gmail.com>
2015-04-09 17:16:44 +0800

    增加了日志
    本日志中一般只记录截图日志，相关的其他日志在Gisto中

--- 2015.mm
+++ 2015.mm
@@ -2,7 +2,7 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1428569673170" ID="ID_1518693960" MODIFIED="1428569723887" TEXT="2015">
 <node CREATED="1428569727266" ID="ID_1324231123" MODIFIED="1428569738184" POSITION="right" TEXT="&#x56db;&#x6708;&#x4efd;">
-<node CREATED="1428569740285" ID="ID_1241415746" MODIFIED="1428569740285" TEXT=""/>
+<node CREATED="1428569740285" ID="ID_1241415746" LINK="2015/04/&#x5173;&#x4e8e;&#x811a;&#x672c;&#x7684;&#x5f53;&#x524d;&#x76ee;&#x5f55;.jpg" MODIFIED="1428570927457" TEXT="0409&#xff1a;&#x811a;&#x672c;&#x4e2d;&#x5f53;&#x524d;&#x76ee;&#x5f55;&#x7684;&#x63d0;&#x53d6;"/>
 </node>
 </node>
 </map>
