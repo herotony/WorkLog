f0efc03a1c8bf953002f6caa39dac7b43148714c
herotony <herotony@gmail.com>
2016-01-06 11:04:38 +0800

    daily record

--- 2016.mm
+++ 2016.mm
@@ -15,7 +15,7 @@
 <node CREATED="1451899376883" ID="ID_863356936" MODIFIED="1451899399683" TEXT="spring transaction manager + spring aop"/>
 </node>
 </node>
-<node CREATED="1451958116278" ID="ID_263759416" MODIFIED="1451985331815" TEXT="1.5">
+<node CREATED="1451958116278" FOLDED="true" ID="ID_263759416" MODIFIED="1452047680242" TEXT="1.5">
 <node CREATED="1451958148482" FOLDED="true" ID="ID_1698390474" MODIFIED="1451977561880" TEXT="&#x90e8;&#x7f72;shshop">
 <node CREATED="1451958120487" ID="ID_1901092819" MODIFIED="1451958143001" TEXT="&#x6302;vpn&#x4e0b;&#x8f7d;&#x5230;&#x5b8c;&#x6574;&#x7684;&#x6700;&#x65b0;&#x7248;resin&#x5305;&#xff0c;&#x4e0d;&#x662f;pro&#x7248;"/>
 <node BACKGROUND_COLOR="#ffffcc" CREATED="1451960602122" ID="ID_173854964" MODIFIED="1451961672983" TEXT="tar&#x6307;&#x4ee4;&#x7684;&#x53c2;&#x6570;&#x987a;&#x5e8f;&#x5f88;&#x4e25;&#x683c;&#xff0c;&#x611f;&#x89c9;&#xff0c;&#x7528;tar -zxvf &#x5c31;&#x80fd;&#x6210;&#x529f;&#x89e3;&#x538b;&#xff0c;&#x7528;tar -xvfz&#x5c31;&#x5931;&#x8d25;...,&#x786e;&#x5b9a;&#x7684;&#x662f;f&#x53c2;&#x6570;&#x5fc5;&#x987b;&#x5728;&#x6700;&#x540e;&#xff0c;&#x5176;&#x4f59;&#x53c2;&#x6570;&#x987a;&#x5e8f;&#x4e0d;&#x4e25;&#x683c;&#xa;&#xa;&#x4f46;&#x89e3;&#x538b;&#x5230;&#x6307;&#x5b9a;&#x76ee;&#x5f55;&#x8fd8;&#x4e0d;&#x6210;&#x529f;&#xff0c;&#x7ee7;&#x7eed;...&#xa;&#xa;&#x76ee;&#x524d;&#x53d8;&#x901a;&#x7684;&#x529e;&#x6cd5;&#x5c31;&#x662f;cd&#x5230;&#x8981;&#x89e3;&#x538b;&#x7684;&#x76ee;&#x6807;&#x76ee;&#x5f55;&#xff0c;&#x7136;&#x540e;&#x7528;tar -zxvf  /...../...tar.gz&#x5373;&#x53ef;&#x3002;"/>
@@ -27,6 +27,30 @@
 </node>
 </node>
 <node CREATED="1451985331816" ID="ID_157578763" MODIFIED="1451985366523" TEXT="&#x660e;&#x5929;&#x4e0a;&#x5348;9&#x70b9;&#x524d;&#x914d;&#x7f6e;&#x597d;&#x5f00;&#x53d1;&#x8054;&#x8c03;&#x7684;&#x524d;&#x7aef;&#x5e76;&#x542f;&#x52a8;&#x3002;"/>
+</node>
+<node CREATED="1452047682703" ID="ID_279130216" MODIFIED="1452047685665" TEXT="1.6">
+<node CREATED="1452047687548" FOLDED="true" ID="ID_773146318" LINK="2016/01/resin-web-deploy.png" MODIFIED="1452049417629" TEXT="resin&#x90e8;&#x7f72;&#x95e8;&#x5e97;&#x641c;&#x7d22;&#x5b8c;&#x6210;&#xff0c;&#x7b80;&#x5355;&#x8bb0;&#x5f55;&#xff0c;&#x770b;&#x56fe;&#x7247;&#x94fe;&#x63a5;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ps -ef |grep esbizcenter-shop|grep java |grep -v grep| awk '{pring $2}' | xargs kill -9
+    </p>
+    <p>
+      &#24378;&#26432;&#21253;&#21547;java ,esbizcenter-shop&#20449;&#24687;&#30340;&#36827;&#31243;
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1452048143379" ID="ID_785542553" MODIFIED="1452048186561" TEXT="grep &#x662f;&#x7b5b;&#x9009;   &#xa;ps -ef | grep shopcenter2   &#x7b5b;&#x9009;&#x51fa; &#x5305;&#x542b;shopcenter2  &#x8fd9;&#x4e2a;&#x7684;&#x8fdb;&#x7a0b; &#xa;ps -ef | grep shopcenter2 |grep java &#x518d;&#x6309;&#x7167;java&#x7b5b;&#x9009; &#xa;ps -ef | grep shopcenter2 |grep java| grep -v grep  &#x6392;&#x9664;&#x7b5b;&#x9009;&#x8fdb;&#x7a0b;&#xff08;&#x5305;&#x542b;grep&#x7684;&#x4e5f;&#x5c31;&#x662f;&#x5f53;&#x524d;&#x67e5;&#x8be2;&#x8fdb;&#x7a0b;&#xff09; &#xa;awk &apos;{print $2}&apos; &#x8f93;&#x51fa;&#x8fdb;&#x7a0b;&#x53f7;  &#xa;xargs kill -9  &#x6740;&#x6389;&#x8fdb;&#x7a0b;"/>
+<node CREATED="1452048197584" ID="ID_435269964" MODIFIED="1452048281657" TEXT="grep&#x672c;&#x8eab;&#x4e5f;&#x662f;&#x4e2a;&#x8fdb;&#x7a0b;&#xff0c;&#x90a3;&#x4e48;grep -v grep&#x5c31;&#x662f;&#x6392;&#x9664;&#x5305;&#x542b;grep&#x5b57;&#x7b26;&#x4fe1;&#x606f;&#x7684;&#x8fdb;&#x7a0b;&#xff0c;-v&#x662f;&#x4e0d;&#x5339;&#x914d;&#xff0c;inverted match&#x7684;&#x610f;&#x601d;&#xff0c;&#x6240;&#x4ee5;grep -v grep&#x5c31;&#x662f;&#x53bb;&#x9664;&quot;grep&#x8fdb;&#x7a0b;&quot;&#x540e;&#x5269;&#x4e0b;&#x7684;&#x8fdb;&#x7a0b;"/>
+<node CREATED="1452048298053" ID="ID_1534546243" MODIFIED="1452048344440" TEXT="kill -9 &#x53c2;&#x6570;9&#x662f;&#x5f3a;&#x5236;&#x6740;&#x8fdb;&#x7a0b;&#x7684;&#x610f;&#x601d;&#xff0c;&#x5c11;&#x7528;&#x4e3a;&#x597d;&#x5427;&#xff0c;kill -9 &#x5c31;&#x662f;&#x5c06;&#x4fe1;&#x53f7;9&#x53d1;&#x9001;&#x7ed9;&#x6307;&#x5b9a;&#x8fdb;&#x7a0b;ID&#x7684;&#x8fdb;&#x7a0b;&#x6765;&#x7ed3;&#x675f;&#x8fdb;&#x7a0b;"/>
+<node CREATED="1452048346881" ID="ID_213820448" MODIFIED="1452048374376" TEXT="awk &apos;{pring $2}&apos;&#x6253;&#x5370;&#x8f93;&#x51fa;&#x8bb0;&#x5f55;&#x7684;&#x7b2c;&#x4e8c;&#x5217;&#x4fe1;&#x606f;&#xff0c;&#x8fd9;&#x91cc;&#x662f;&#x8fdb;&#x7a0b;ID"/>
+<node CREATED="1452048378778" ID="ID_1731958504" MODIFIED="1452048393328" TEXT="ps -ef &#x663e;&#x793a;&#x6240;&#x6709;&#x8fdb;&#x7a0b;"/>
+<node BACKGROUND_COLOR="#ffffcc" COLOR="#660000" CREATED="1452049188524" ID="ID_1835983416" MODIFIED="1452049413738" TEXT="xargs&#x662f;&#x5bf9;&#x6240;&#x6709;&#x7684;&#x5220;&#x9009;&#x7ed3;&#x679c;&#xff0c;&#x6309;&#x6b21;&#x5e8f;&#x4e00;&#x4e2a;&#x4e00;&#x4e2a;&#x5f53;&#x4f5c;&#x3010;&#x53c2;&#x6570;&#x3011;&#x4ea4;&#x7ed9;&#x76f8;&#x5e94;&#x7684;&#x547d;&#x4ee4;&#x8fdb;&#x884c;&#x540e;&#x7eed;&#x5904;&#x7406;&#xff0c;&#xa;&#xa;1&#x3001;&#x5728;&#x8fd9;&#x91cc;&#x7684;&#x547d;&#x4ee4;&#x662f;kill -9&#x7684;&#x5904;&#x7406;&#xa;2&#x3001;&#x53c2;&#x6570;&#x662f;&#x7b5b;&#x9009;&#x51fa;&#x6765;&#x7684;&#x8fdb;&#x7a0b;ID&#xa;3&#x3001;&#x5bf9;&#x6240;&#x6709;&#x8fdb;&#x7a0b;ID&#xff0c;&#x4e00;&#x4e2a;&#x4e2a;&#x4f9d;&#x6b21;&#x8fdb;&#x884c;kill -9 &#x7684;&#x547d;&#x4ee4;&#x5904;&#x7406;&#xff0c;&#x8fd9;&#x5c31;&#x662f;xargs&#x7684;&#x7528;&#x5904;&#xa;"/>
+</node>
 </node>
 </node>
 </node>
