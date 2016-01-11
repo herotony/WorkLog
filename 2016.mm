ca12a0898347ca5e285a125a91650a4009f61c27
herotony <herotony@gmail.com>
2016-01-11 17:57:38 +0800

    daily record

--- 2016.mm
+++ 2016.mm
@@ -4,7 +4,7 @@
 <node CREATED="1451869143342" ID="ID_1042502935" LINK="2015.mm" MODIFIED="1451869172278" POSITION="left" TEXT="gobackto 2015"/>
 <node CREATED="1451869185884" ID="ID_557272657" MODIFIED="1451869194693" POSITION="right" TEXT="&#x4e00;&#x5b63;&#x5ea6;">
 <node CREATED="1451869199225" ID="ID_1396695593" MODIFIED="1451869205392" TEXT="&#x4e00;&#x6708;">
-<node CREATED="1451869208768" ID="ID_242997966" MODIFIED="1451869249509" TEXT="&#x7b2c;&#x4e8c;&#x5468;">
+<node CREATED="1451869208768" FOLDED="true" ID="ID_242997966" MODIFIED="1452506168984" TEXT="&#x7b2c;&#x4e8c;&#x5468;">
 <node CREATED="1451869252942" FOLDED="true" ID="ID_157413133" MODIFIED="1452062237229" TEXT="1.4">
 <node CREATED="1451869290875" FOLDED="true" ID="ID_1539491480" MODIFIED="1452062236252" TEXT="&#x5176;&#x4ed6;">
 <node CREATED="1451869263260" ID="ID_1544260454" MODIFIED="1451869282260" TEXT="7:00&#x524d;&#x5230;&#x516c;&#x53f8;&#xff0c;&#x8d77;&#x98ce;&#xff0c;&#x7a7a;&#x6c14;&#x5fe7;"/>
@@ -15,7 +15,7 @@
 <node CREATED="1451899376883" ID="ID_863356936" MODIFIED="1451899399683" TEXT="spring transaction manager + spring aop"/>
 </node>
 </node>
-<node CREATED="1451958116278" FOLDED="true" ID="ID_263759416" MODIFIED="1452125702209" TEXT="1.5">
+<node CREATED="1451958116278" FOLDED="true" ID="ID_263759416" MODIFIED="1452502846057" TEXT="1.5">
 <node CREATED="1451958148482" FOLDED="true" ID="ID_1698390474" MODIFIED="1451977561880" TEXT="&#x90e8;&#x7f72;shshop">
 <node CREATED="1451958120487" ID="ID_1901092819" MODIFIED="1451958143001" TEXT="&#x6302;vpn&#x4e0b;&#x8f7d;&#x5230;&#x5b8c;&#x6574;&#x7684;&#x6700;&#x65b0;&#x7248;resin&#x5305;&#xff0c;&#x4e0d;&#x662f;pro&#x7248;"/>
 <node BACKGROUND_COLOR="#ffffcc" CREATED="1451960602122" ID="ID_173854964" MODIFIED="1451961672983" TEXT="tar&#x6307;&#x4ee4;&#x7684;&#x53c2;&#x6570;&#x987a;&#x5e8f;&#x5f88;&#x4e25;&#x683c;&#xff0c;&#x611f;&#x89c9;&#xff0c;&#x7528;tar -zxvf &#x5c31;&#x80fd;&#x6210;&#x529f;&#x89e3;&#x538b;&#xff0c;&#x7528;tar -xvfz&#x5c31;&#x5931;&#x8d25;...,&#x786e;&#x5b9a;&#x7684;&#x662f;f&#x53c2;&#x6570;&#x5fc5;&#x987b;&#x5728;&#x6700;&#x540e;&#xff0c;&#x5176;&#x4f59;&#x53c2;&#x6570;&#x987a;&#x5e8f;&#x4e0d;&#x4e25;&#x683c;&#xa;&#xa;&#x4f46;&#x89e3;&#x538b;&#x5230;&#x6307;&#x5b9a;&#x76ee;&#x5f55;&#x8fd8;&#x4e0d;&#x6210;&#x529f;&#xff0c;&#x7ee7;&#x7eed;...&#xa;&#xa;&#x76ee;&#x524d;&#x53d8;&#x901a;&#x7684;&#x529e;&#x6cd5;&#x5c31;&#x662f;cd&#x5230;&#x8981;&#x89e3;&#x538b;&#x7684;&#x76ee;&#x6807;&#x76ee;&#x5f55;&#xff0c;&#x7136;&#x540e;&#x7528;tar -zxvf  /...../...tar.gz&#x5373;&#x53ef;&#x3002;"/>
@@ -28,8 +28,8 @@
 </node>
 <node CREATED="1451985331816" ID="ID_157578763" MODIFIED="1451985366523" TEXT="&#x660e;&#x5929;&#x4e0a;&#x5348;9&#x70b9;&#x524d;&#x914d;&#x7f6e;&#x597d;&#x5f00;&#x53d1;&#x8054;&#x8c03;&#x7684;&#x524d;&#x7aef;&#x5e76;&#x542f;&#x52a8;&#x3002;"/>
 </node>
-<node BACKGROUND_COLOR="#ccffcc" CREATED="1452047682703" ID="ID_279130216" MODIFIED="1452160052990" TEXT="1.6">
-<node CREATED="1452047687548" FOLDED="true" ID="ID_773146318" LINK="2016/01/resin-web-deploy.png" MODIFIED="1452125599394" TEXT="resin&#x90e8;&#x7f72;&#x95e8;&#x5e97;&#x641c;&#x7d22;&#x5b8c;&#x6210;&#xff0c;&#x7b80;&#x5355;&#x8bb0;&#x5f55;&#xff0c;&#x770b;&#x56fe;&#x7247;&#x94fe;&#x63a5;">
+<node BACKGROUND_COLOR="#ccffcc" CREATED="1452047682703" FOLDED="true" ID="ID_279130216" MODIFIED="1452502868693" TEXT="1.6">
+<node CREATED="1452047687548" FOLDED="true" ID="ID_773146318" LINK="2016/01/resin-web-deploy.png" MODIFIED="1452502841178" TEXT="resin&#x90e8;&#x7f72;&#x95e8;&#x5e97;&#x641c;&#x7d22;&#x5b8c;&#x6210;&#xff0c;&#x7b80;&#x5355;&#x8bb0;&#x5f55;&#xff0c;&#x770b;&#x56fe;&#x7247;&#x94fe;&#x63a5;">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -53,14 +53,14 @@
 <node CREATED="1452125565539" ID="ID_1345166655" MODIFIED="1452125594434" TEXT="&#x5f00;&#x4f1a;&#x8981;&#x6c42;996&#x4e86;&#xff0c;&#x8fd9;&#x6bb5;&#x65f6;&#x95f4;&#x591a;&#x5b66;&#x4e60;&#x5427;&#xff0c;&#x56de;&#x5bb6;&#x76f4;&#x63a5;&#x7761;&#x89c9;&#x4e86;..."/>
 <node CREATED="1452125620073" ID="ID_1796853141" MODIFIED="1452125664855" TEXT="&#x731b;&#x8f6c;&#x5934;&#x770b;&#x65c1;&#x8fb9;&#x7684;&#x5c4f;&#x5e55;&#x4f1a;&#x6709;&#x4e00;&#x4e2a;&#x8fc7;&#x5ea6;&#x611f;&#x89c9;&#x50cf;&#x91cd;&#x5f71;&#x3002;&#x3002;&#x3002;&#x65e5;&#x4e86;"/>
 </node>
-<node CREATED="1452125611040" FOLDED="true" ID="ID_1799988121" MODIFIED="1452237387603" TEXT="1.7">
+<node CREATED="1452125611040" FOLDED="true" ID="ID_1799988121" MODIFIED="1452502872058" TEXT="1.7">
 <node CREATED="1452125670296" ID="ID_952390283" MODIFIED="1452154994512" TEXT="&#x7ee7;&#x7eed;notifyserver console&#x90e8;&#x5206;">
 <node CREATED="1452131514153" ID="ID_538506054" LINK="2016/01/web-xml.png" MODIFIED="1452131528139" TEXT="webxml"/>
 <node CREATED="1452154997471" ID="ID_544872769" LINK="2016/01/java-arg-desc.png" MODIFIED="1452155016992" TEXT="java&#x53c2;&#x6570;&#x6574;&#x7406;"/>
 </node>
 <node CREATED="1452163023365" ID="ID_1145457004" MODIFIED="1452163059668" TEXT="&#x4e0d;&#x770b;notifyserver&#x4ee3;&#x7801;&#x4e86;&#xff0c;&#x6539;&#x5168;&#x529b;&#x642d;&#x5efa;&#x672c;&#x5730;&#x73af;&#x5883;&#x5427;&#xff0c;&#x4e89;&#x53d6;&#x672c;&#x5468;&#x641e;&#x5b9a;&#x5427;"/>
 </node>
-<node CREATED="1452237389721" FOLDED="true" ID="ID_1642886528" MODIFIED="1452320269937" TEXT="1.8">
+<node CREATED="1452237389721" FOLDED="true" ID="ID_1642886528" MODIFIED="1452502878970" TEXT="1.8">
 <node CREATED="1452237395726" ID="ID_634218149" MODIFIED="1452237439018" TEXT="&#x641e;&#x5b9a;&#x4e86;&#x865a;&#x673a;&#x548c;host&#x4e92;&#x8bbf;&#x95ee;&#x9898;&#xff0c;&#x4e3b;&#x8981;&#x662f;centos&#x7684;&#x9632;&#x706b;&#x5899;&#x5bfc;&#x81f4;&#x5427;&#xff0c;&#x5176;&#x4f59;&#x7684;&#x95ee;&#x9898;&#x5f85;&#x6574;&#x7406;&#xff0c;&#x5728;onenote/java&#x4e2d;&#x6709;"/>
 <node CREATED="1452237442328" ID="ID_1537834887" MODIFIED="1452237544874" TEXT="&#x5e74;&#x524d;&#xff0c;&#x4fee;&#x6539;esbizcenter-shop&#xff0c;&#x652f;&#x6301;&#x6839;&#x636e;&#x95e8;&#x5e97;id&#x67e5;&#x8be2;&#x76f8;&#x5173;&#x8bb0;&#x5f55;&#xff0c;&#x53e6;&#x5916;&#xff0c;&#x8003;&#x8651;&#x505a;&#x81ea;&#x52a8;&#x5316;&#x811a;&#x672c;&#x6765;&#x4e00;&#x6b21;&#x6027;&#x5b89;&#x88c5;&#x542f;&#x52a8;ssh,resin,mysql,redis,mongo&#x7b49;&#x7b49;&#x5de5;&#x4f5c;&#xff0c;&#x4fbf;&#x4e8e;&#x540e;&#x7eed;&#x53cd;&#x590d;&#x5feb;&#x901f;&#x642d;&#x5efa;&#x672c;&#x5730;&#x5f00;&#x53d1;&#x73af;&#x5883;"/>
 <node CREATED="1452243289458" ID="ID_283351742" MODIFIED="1452243312801" TEXT="&#x7528;nc -v  ip port&#x6765;&#x6d4b;&#x8bd5;&#x662f;&#x5426;&#x8fde;&#x63a5;&#x901a;">
@@ -70,9 +70,9 @@
 <node CREATED="1452244333660" ID="ID_775993514" MODIFIED="1452244405592" TEXT="du -sh &#x6307;&#x4ee4;&#x67e5;&#x8be2;&#x5f53;&#x524d;&#x76ee;&#x5f55;&#x78c1;&#x76d8;&#x4f7f;&#x7528;&#x7387;,disk usage"/>
 <node CREATED="1452250447181" ID="ID_1856591550" MODIFIED="1452250466224" TEXT="&#x5b89;&#x88c5;&#x4e86;redis&#xff0c;&#x660e;&#x5929;&#x7ee7;&#x7eed;&#x6d4b;&#xff0c;&#x540c;&#x65f6;&#x88c5;&#x4e0a;mysql"/>
 </node>
-<node CREATED="1452320276339" FOLDED="true" ID="ID_368578008" MODIFIED="1452329837569" TEXT="1.9">
+<node CREATED="1452320276339" FOLDED="true" ID="ID_368578008" MODIFIED="1452506031082" TEXT="1.9">
 <node CREATED="1452320282867" ID="ID_810329020" LINK="2016/01/bash%20-%20if%20-square.jpg" MODIFIED="1452320441149" TEXT="sh&#x811a;&#x672c;&#x7684;&#x975e;&#x5e38;&#x7b80;&#x5355;&#x7684;if/elif/fi&#x6761;&#x4ef6;&#x5224;&#x65ad;&#xff0c;&#x88ab;[]&#x5751;&#x4e86;&#x4e00;&#x5929;&#xff0c;&#x539f;&#x56e0;&#x5c31;&#x662f;&#x56e0;&#x4e3a;[]&#x7528;&#x4e8e;&#x6b63;&#x5219;&#x7b49;&#x5f88;&#x591a;&#x5730;&#x65b9;&#xff0c;&#xa;&#x4f5c;&#x4e3a;&#x5224;&#x65ad;&#x5f0f;&#x5219;&#x5fc5;&#x987b;&#x8981;&#x5728;&#x6240;&#x6709;&#x76f8;&#x5173;&#x5730;&#x65b9;&#x52a0;&#x7a7a;&#x683c;&#xff0c;&#x53c2;&#x8003;&#x56fe;&#x793a;&#xa;&#xa;&#x5c24;&#x5176;&#x662f;&#x201c;&#x7b49;&#x53f7;&#x201d;&#x4e24;&#x8fb9;&#x7684;&#x7a7a;&#x683c;&#xff0c;&#x65e5;&#x4e86;&#x72d7;&#x4e86;&#xff01;"/>
-<node CREATED="1452324050713" FOLDED="true" ID="ID_1145696836" MODIFIED="1452329827512" TEXT="&#x5c06;redis&#x7684;conf&#x6587;&#x4ef6;&#x4e2d;daemon&#x6a21;&#x5f0f;&#x8c03;&#x4e3a;yes&#x540e;&#xff0c;&#x5c31;&#x80fd;&#x6b63;&#x5e38;&#x7528;./redis-cli &#x6765;&#x6d4b;&#x8bd5;&#x4e86;&#xff0c;&#x4ea4;&#x4e92;&#x6a21;&#x5f0f;&#x4e2d;&#x8f93;&#x5165;shutdown&#x53ef;&#x5173;&#x95ed;daemon&#x6a21;&#x5f0f;&#x4e0b;&#x7684;redis server&#xff0c;&#x5728;redis server&#x524d;&#x7aef;&#x6a21;&#x5f0f;&#x4e0b;&#x5219;&#x7528;ctrl -C &#x6765;&#x5173;&#x95ed;&#x670d;&#x52a1;,&#x5176;&#x4f59;&#x914d;&#x7f6e;&#x67e5;&#x770b;evernote&#x7b14;&#x8bb0;&#x5373;&#x53ef;">
+<node CREATED="1452324050713" FOLDED="true" ID="ID_1145696836" MODIFIED="1452502808345" TEXT="&#x5c06;redis&#x7684;conf&#x6587;&#x4ef6;&#x4e2d;daemon&#x6a21;&#x5f0f;&#x8c03;&#x4e3a;yes&#x540e;&#xff0c;&#x5c31;&#x80fd;&#x6b63;&#x5e38;&#x7528;./redis-cli &#x6765;&#x6d4b;&#x8bd5;&#x4e86;&#xff0c;&#x4ea4;&#x4e92;&#x6a21;&#x5f0f;&#x4e2d;&#x8f93;&#x5165;shutdown&#x53ef;&#x5173;&#x95ed;daemon&#x6a21;&#x5f0f;&#x4e0b;&#x7684;redis server&#xff0c;&#x5728;redis server&#x524d;&#x7aef;&#x6a21;&#x5f0f;&#x4e0b;&#x5219;&#x7528;ctrl -C &#x6765;&#x5173;&#x95ed;&#x670d;&#x52a1;,&#x5176;&#x4f59;&#x914d;&#x7f6e;&#x67e5;&#x770b;evernote&#x7b14;&#x8bb0;&#x5373;&#x53ef;">
 <node CREATED="1452325044477" ID="ID_472400908" MODIFIED="1452325211560">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -83,14 +83,47 @@
       <b><font color="#cc3300">./redis-cli </font></b>-h 127.0.0.1 -p 6379&#160;&#160;get foo<br /><br /><font color="#660099">-h:&#20195;&#34920;host&#65292;&#36830;&#25509;&#36828;&#31243;&#25165;&#29992;<br color="#660099" />-p:&#20195;&#34920;port&#65292;&#33258;&#23450;&#20041;&#31471;&#21475;&#25165;&#29992;<br color="#660099" />get foo:&#20855;&#20307;&#21521;redis server&#21457;&#36865;&#30340;&#25351;&#20196;&#25968;&#25454;</font><br /><br />
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
-<node CREATED="1452329154161" ID="ID_57735707" MODIFIED="1452329162790" TEXT="&#x4e24;&#x6bb5;&#x811a;&#x672c;">
-<node CREATED="1452329164495" ID="ID_1848298498" LINK="2016/01/chk-ps-list-sh.jpg" MODIFIED="1452329801193" TEXT="&#x67e5;&#x8be2;&#x6307;&#x5b9a;&#x540d;&#x79f0;&#x7684;&#x8fdb;&#x7a0b;&#x7684;PID"/>
+<node CREATED="1452329154161" ID="ID_57735707" MODIFIED="1452505975587" TEXT="&#x4e24;&#x6bb5;&#x811a;&#x672c;">
+<node CREATED="1452329164495" ID="ID_1848298498" LINK="2016/01/chk-ps-list-sh.jpg" MODIFIED="1452329801193" TEXT="&#x67e5;&#x8be2;&#x6307;&#x5b9a;&#x540d;&#x79f0;&#x7684;&#x8fdb;&#x7a0b;&#x7684;PID">
+<node CREATED="1452505996157" ID="ID_1781705607" MODIFIED="1452506012696" TEXT="&#x8fd0;&#x884c;&#x6a21;&#x5f0f;&#x5f88;&#x7b80;&#x5355;&#xff1a;./chk-ps-list  mysql  notifyserver  ssh ..."/>
+</node>
 <node CREATED="1452329194036" ID="ID_1372812509" LINK="2016/01/start-local-env-sh.jpg" MODIFIED="1452329793520" TEXT="&#x542f;&#x52a8;&#x5173;&#x95ed;&#x4e00;&#x7cfb;&#x5217;&#x76f8;&#x5173;&#x670d;&#x52a1;&#xff0c;&#x8fd9;&#x91cc;&#x662f;redis&#x548c;resin"/>
 </node>
+</node>
+</node>
+<node CREATED="1452495205758" ID="ID_1384368266" MODIFIED="1452495214316" TEXT="&#x7b2c;&#x4e09;&#x5468;">
+<node CREATED="1452495216040" FOLDED="true" ID="ID_1536553854" MODIFIED="1452506169824" TEXT="1.11">
+<node CREATED="1452495220233" FOLDED="true" ID="ID_1659188081" MODIFIED="1452506164801" TEXT="mysql&#x7528;500&#x6765;&#x5146;&#x7684;rpm&#x5927;&#x5305;&#x6765;&#x5b89;&#x88c5;&#x5931;&#x8d25;&#xff0c;&#x53cd;&#x800c;&#x7528;yum install mysql-community-server&#x6210;&#x529f;&#xff0c;&#xa;&#xa;&#x6b64;&#x524d;&#x8981;&#x5b89;&#x88c5;&#x4e00;&#x4e2a;8k&#x7684;rpm&#x5e93;&#xff0c;&#x4ea6;&#x5373;&#x786e;&#x5b9a;mysql&#x7684;&#x5b89;&#x88c5;&#x6e90;&#x5730;&#x5740;&#xa;&#xa;rpm -Uvh http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm">
+<node CREATED="1452506042367" ID="ID_1166549828" MODIFIED="1452506161857" TEXT="mysql&#x901a;&#x8fc7;systemctl enable msyqld&#x8bbe;&#x7f6e;&#x4e3a;&#x81ea;&#x52a8;&#x542f;&#x52a8;&#x4e86;&#xa;&#xa;systemctl status msyqld&#x67e5;&#x8be2;&#x72b6;&#x6001;&#xa;systemctl start mysqld&#x542f;&#x52a8;mysql&#xa;systemctl stop mysqld &#x505c;&#x6b62;&#x670d;&#x52a1;"/>
+</node>
+<node CREATED="1452502439470" FOLDED="true" ID="ID_1863767134" MODIFIED="1452505966856">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#23433;&#35013;&#21518;&#65292;&#36827;&#20837;<font color="#3300cc">/var/log/mysqld.log</font>&#25628;&#32034;<b><font color="#990000">temproary password</font></b>&#21487;&#30475;&#21040;&#21021;&#22987;&#21270;&#23494;&#30721;...&#22353;&#21834;!!!
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1452503904325" ID="ID_117721161" MODIFIED="1452504394014">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#28982;&#21518;&#29992; mysql -u root -p<br /><br />&#36755;&#20837;&#20020;&#26102;&#23494;&#30721;&#30331;&#24405;&#21518;&#65292;&#25353;mysql&#35201;&#27714;&#24517;&#39035;&#37325;&#32622;&#23494;&#30721;&#65292;&#29992;&#22914;&#19979;&#21629;&#20196;<br /><br /><font color="#009933">SET PASSWORD = PASSWORD('W_dl1212231');</font><br /><br />&#20462;&#25913;&#23436;&#25104;root&#30340;&#26032;&#23494;&#30721;<br /><br />&#20877;&#20462;&#25913;mysql - user&#34920;&#30340;host&#20026; %&#20197;&#20801;&#35768;&#20854;&#20182;&#26426;&#22120;&#36828;&#31243;&#30331;&#20837;<br /><br /><font color="#009933">use mysql;<br color="#009933" />update user set host='%' where user='root';<br color="#009933" />flush privileges;<br color="#009933" />quit;</font><br />
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1452505867308" ID="ID_492248954" MODIFIED="1452505948585" TEXT="&#x5728;/etc/rc.d/rc.local&#x4e2d;&#x52a0;&#x5165;&#x4e86;&#x81ea;&#x52a8;mount&#x5171;&#x4eab;&#x76ee;&#x5f55;Tool : mount -t vboxsf Tool /mnt/tool&#xff0c;&#x540c;&#x65f6;&#x8d4b;&#x4e88;rc.local&#x6267;&#x884c;&#x6743;&#x9650;"/>
 </node>
 </node>
 </node>
