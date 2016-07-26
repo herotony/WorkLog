42dd033826787f0bde111be1b3ee8c0e2ddca9d1
herotony <herotony@gmail.com>
2016-07-26 14:58:55 +0800

    add new file

---
+++ Spring MVC.mm
@@ -0,0 +1,68 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1469420178465" ID="ID_1471626700" MODIFIED="1469420190587" TEXT="Spring MVC">
+<node CREATED="1469501968782" ID="ID_1467135574" MODIFIED="1469501990006" POSITION="right" TEXT="&#x4e3b;&#x7ad9;&#x90e8;&#x5206;&#x6e90;&#x7801;&#x5206;&#x6790;">
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1469502005067" ID="ID_34380387" MODIFIED="1469502396271" TEXT="cozy-framework-service">
+<node CREATED="1469502036141" ID="ID_977131218" MODIFIED="1469502478109" TEXT="template">
+<node CREATED="1469502045854" ID="ID_1177059716" LINK="https://gist.github.com/herotony/848f9b5841bbdb113d8e4894af48d562/#file-callresult-java" MODIFIED="1469503507615" TEXT="CallResult">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#23450;&#20041;&#20102;&#32479;&#19968;&#30340;&#22788;&#29702;&#32467;&#26524;&#36820;&#22238;&#27169;&#24335;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1469502327021" ID="ID_1259347602" MODIFIED="1469502354647" TEXT="interface">
+<node CREATED="1469502334885" ID="ID_260791813" LINK="https://gist.github.com/herotony/848f9b5841bbdb113d8e4894af48d562/#file-servicetemplate-java" MODIFIED="1469503678816" TEXT="ServiceTemplate">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#35774;&#23450;&#20102;&#25968;&#25454;&#24211;&#25191;&#34892;&#30340;&#25968;&#25454;&#28304;&#21644;&#26159;&#21542;&#25903;&#25345;&#20107;&#21153;&#65292;&#26412;&#25509;&#21475;&#26356;&#24212;&#35813;&#21483;databaseTemplate
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1469502356552" ID="ID_1683216815" LINK="https://gist.github.com/herotony/848f9b5841bbdb113d8e4894af48d562/#file-templateaction-java" MODIFIED="1469503654753" TEXT="TemplateAction">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#21807;&#19968;&#26041;&#27861;doAction&#65292;&#29992;&#20110;&#22788;&#29702;&#20219;&#20309;&#19994;&#21153;&#36923;&#36753;&#65292;&#21069;&#32622;&#26377;&#21442;&#25968;&#26657;&#39564;&#65292;&#19994;&#21153;&#26657;&#39564;&#65292;&#21518;&#32622;&#26377;&#25195;&#23614;&#28165;&#29702;&#12290;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1469502480037" ID="ID_1294473970" LINK="https://gist.github.com/herotony/848f9b5841bbdb113d8e4894af48d562/#file-codeutil-java" MODIFIED="1469503705303" TEXT="CodeUtil">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#35828;&#26159;&#35299;&#26512;CallResult&#30340;code
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1469503309520" ID="ID_551569564" MODIFIED="1469503319533" TEXT="abstract class">
+<node CREATED="1469503321632" ID="ID_1422580002" LINK="https://gist.github.com/herotony/848f9b5841bbdb113d8e4894af48d562/#file-abstracttemplateaction-java" MODIFIED="1469503349657" TEXT="AbstractTemplateAction"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
