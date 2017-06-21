24d9178a6f310096e1883ec35b3dca3c14f1aee7
herotony <herotony@gmail.com>
2017-06-21 15:42:39 +0800

    加入atom工具部分style个性化设置

--- Self-Note-from2017.mm
+++ Self-Note-from2017.mm
@@ -13,7 +13,7 @@
 </html>
 </richcontent>
 <node CREATED="1497949648942" ID="ID_1067471078" MODIFIED="1497949653574" POSITION="right" TEXT="2017">
-<node CREATED="1497949655242" ID="ID_797164255" MODIFIED="1497949818228">
+<node CREATED="1497949655242" ID="ID_797164255" MODIFIED="1498024955998">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -25,7 +25,7 @@
   </body>
 </html>
 </richcontent>
-<node CREATED="1497949687673" FOLDED="true" ID="ID_1068750738" MODIFIED="1497949894011" TEXT="target">
+<node CREATED="1497949687673" FOLDED="true" ID="ID_1068750738" MODIFIED="1498024375894" TEXT="target">
 <node CREATED="1497949735966" ID="ID_1710668953" MODIFIED="1497949892406">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -48,13 +48,14 @@
 <node CREATED="1498008064404" ID="ID_1652616388" MODIFIED="1498008064404" TEXT=""/>
 </node>
 <node CREATED="1497949818229" ID="ID_1193604841" MODIFIED="1497949820431" TEXT="22"/>
+<node CREATED="1498024956001" ID="ID_1748484097" MODIFIED="1498024956001" TEXT=""/>
 </node>
 </node>
 <node CREATED="1497950447925" ID="ID_97374355" LINK="https://guides.github.com/features/mastering-markdown/" MODIFIED="1497950470987" POSITION="left" TEXT="how to use markdown on github"/>
 <node CREATED="1497950566687" ID="ID_1811308198" LINK="https://gist.github.com/" MODIFIED="1497950587712" POSITION="left" TEXT="&#x76f4;&#x63a5;&#x8fdb;&#x5165;&#x6211;&#x7684;Gist"/>
 <node BACKGROUND_COLOR="#ccffcc" COLOR="#009933" CREATED="1498013737760" ID="ID_1907796611" MODIFIED="1498014388905" POSITION="left" TEXT="Atom">
 <node CREATED="1498013775937" ID="ID_1849716924" LINK="https://atom.io/" MODIFIED="1498014325815" TEXT="&#x4e0b;&#x8f7d;&#x5730;&#x5740;"/>
-<node CREATED="1498014247622" FOLDED="true" ID="ID_634546941" MODIFIED="1498014793929">
+<node CREATED="1498014247622" FOLDED="true" ID="ID_634546941" MODIFIED="1498024369590">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -91,6 +92,419 @@
 </node>
 </node>
 <node CREATED="1498013813223" ID="ID_48928566" LINK="http://www.cnblogs.com/fanzhidongyzby/p/6637084.html" MODIFIED="1498013825788" TEXT="&#x7f51;&#x6587;"/>
+<node CREATED="1498024959126" FOLDED="true" ID="ID_1562270160" MODIFIED="1498025887847">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Atom&#37319;&#29992;&#30340;css&#65292;&#22312;open config folder&#20013;
+    </p>
+    <p>
+      &#36873;&#25321;custom your style&#20013;&#30340;&#25991;&#20214;&#20013;&#35774;&#32622;&#21363;&#21487;
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1498025007920" ID="ID_1550140025" MODIFIED="1498025885158">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      @charset &quot;utf-8&quot;;
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      html { font-size: 100%; overflow-y: scroll; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; }
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      body{
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;color:#444;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;font-family:Georgia, Palatino, 'Palatino Linotype', Times, 'Times New Roman', serif;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;font-size:13px;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;line-height:1.5em;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;padding:1em;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;margin:auto;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;background:#fefefe;
+    </p>
+    <p>
+      }
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      h1, h2, h3, h4, h5, h6 {
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;font-weight: bold;
+    </p>
+    <p>
+      }
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      h1 {
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;color: #000000;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;font-size: 28px;
+    </p>
+    <p>
+      }
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      h2 {
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;border-bottom: 2px solid #CCCCCC;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;color: #000000;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;font-size: 24px;
+    </p>
+    <p>
+      }
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      h3 {
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;border-bottom: 2px solid #CCCCCC;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;font-size: 18px;
+    </p>
+    <p>
+      }
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      h4 {
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;font-size: 16px;
+    </p>
+    <p>
+      }
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      h5 {
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;font-size: 14px;
+    </p>
+    <p>
+      }
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      h6 {
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;color: #777777;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;background-color: inherit;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;font-size: 14px;
+    </p>
+    <p>
+      }
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      hr {
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;height: 0.2em;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;border: 0;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;color: #CCCCCC;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;background-color: #CCCCCC;
+    </p>
+    <p>
+      }
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      p, blockquote, ul, ol, dl, li, table, pre {
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;margin: 15px 0;
+    </p>
+    <p>
+      }
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      p{
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;margin:1em 0;
+    </p>
+    <p>
+      }
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      pre {
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;background-color: #F8F8F8;&#160;&#160;&#160;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;border: 1px solid #CCCCCC;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;border-radius: 3px;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;overflow: auto;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;padding: 5px;
+    </p>
+    <p>
+      }
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      pre code {
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;background-color: #F8F8F8;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;border: none;&#160;&#160;&#160;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;padding: 0;
+    </p>
+    <p>
+      }
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      code {
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;font-family: Consolas, Monaco, Andale Mono, monospace;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;background-color:#F8F8F8;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;border: 1px solid #CCCCCC;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;border-radius: 3px;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;padding: 0 0.2em;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;line-height: 1;
+    </p>
+    <p>
+      }
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      pre &gt; code {
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;border: 0;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;margin: 0;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;padding: 0;
+    </p>
+    <p>
+      }
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      a{ color: #0645ad; text-decoration:none;}
+    </p>
+    <p>
+      a:visited{ color: #0b0080; }
+    </p>
+    <p>
+      a:hover{ color: #06e; }
+    </p>
+    <p>
+      a:active{ color:#faa700; }
+    </p>
+    <p>
+      a:focus{ outline: thin dotted; }
+    </p>
+    <p>
+      a:hover, a:active{ outline: 0; }
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      ::-moz-selection{background:rgba(255,255,0,0.3);color:#000}
+    </p>
+    <p>
+      ::selection{background:rgba(255,255,0,0.3);color:#000}
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      a::-moz-selection{background:rgba(255,255,0,0.3);color:#0645ad}
+    </p>
+    <p>
+      a::selection{background:rgba(255,255,0,0.3);color:#0645ad}
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      blockquote{
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;color:#666666;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;margin:0;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;padding-left: 3em;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;border-left: 0.5em #EEE solid;
+    </p>
+    <p>
+      }
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      ul, ol { margin: 1em 0; padding: 0 0 0 2em; }
+    </p>
+    <p>
+      li p:last-child { margin:0 }
+    </p>
+    <p>
+      dd { margin: 0 0 0 2em; }
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      img { border: 0; -ms-interpolation-mode: bicubic; vertical-align: middle; max-width:100%;}
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      table { border-collapse: collapse; border-spacing: 0; }
+    </p>
+    <p>
+      td { vertical-align: top; }
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      @media only screen and (min-width: 480px) {
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;body{font-size:14px;}
+    </p>
+    <p>
+      }
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      @media only screen and (min-width: 768px) {
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;body{font-size:16px;}
+    </p>
+    <p>
+      }
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
 </node>
 </node>
 </map>
