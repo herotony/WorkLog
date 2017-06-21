9a162074e78631b6730efdb065c1391ac5c394d2
herotony <herotony@gmail.com>
2017-06-21 11:08:53 +0800

    加入atom markdown编辑工具说明

---
+++ MDFiles/2017/_drafts/redis-install.markdown
@@ -0,0 +1,12 @@
+---
+layout: post
+title: redis-install
+date: '2017-06-21 10:41'
+tags:
+  - quit
+---
+```Java
+print "hello world"
+
+
+```
--- Self-Note-from2017.mm
+++ Self-Note-from2017.mm
@@ -12,5 +12,85 @@
   </body>
 </html>
 </richcontent>
+<node CREATED="1497949648942" ID="ID_1067471078" MODIFIED="1497949653574" POSITION="right" TEXT="2017">
+<node CREATED="1497949655242" ID="ID_797164255" MODIFIED="1497949818228">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#20845;&#26376;
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1497949687673" FOLDED="true" ID="ID_1068750738" MODIFIED="1497949894011" TEXT="target">
+<node CREATED="1497949735966" ID="ID_1710668953" MODIFIED="1497949892406">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      plan to learn redis for one month
+    </p>
+    <p>
+      step by step
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1497949679644" ID="ID_774567807" MODIFIED="1498008062830" TEXT="21">
+<node CREATED="1497949758241" ID="ID_1029117922" MODIFIED="1497949797119" TEXT="redis install "/>
+<node CREATED="1498008064404" ID="ID_1652616388" MODIFIED="1498008064404" TEXT=""/>
+</node>
+<node CREATED="1497949818229" ID="ID_1193604841" MODIFIED="1497949820431" TEXT="22"/>
+</node>
+</node>
+<node CREATED="1497950447925" ID="ID_97374355" LINK="https://guides.github.com/features/mastering-markdown/" MODIFIED="1497950470987" POSITION="left" TEXT="how to use markdown on github"/>
+<node CREATED="1497950566687" ID="ID_1811308198" LINK="https://gist.github.com/" MODIFIED="1497950587712" POSITION="left" TEXT="&#x76f4;&#x63a5;&#x8fdb;&#x5165;&#x6211;&#x7684;Gist"/>
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#009933" CREATED="1498013737760" ID="ID_1907796611" MODIFIED="1498014388905" POSITION="left" TEXT="Atom">
+<node CREATED="1498013775937" ID="ID_1849716924" LINK="https://atom.io/" MODIFIED="1498014325815" TEXT="&#x4e0b;&#x8f7d;&#x5730;&#x5740;"/>
+<node CREATED="1498014247622" FOLDED="true" ID="ID_634546941" MODIFIED="1498014348257">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      pdf converter&#160;&#25554;&#20214;
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1498013781336" ID="ID_1140559271" LINK="MDFiles/2017/phantomjs-2.1.1-windows.zip" MODIFIED="1498014234652">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      pdf&#25554;&#20214;&#25152;&#32570;&#21253;
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1498014273225" ID="ID_1794238472" MODIFIED="1498014295151">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="MDFiles/2017/images/06/how%20to%20make%20pdfconverter%20right.jpg" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1498013813223" ID="ID_48928566" LINK="http://www.cnblogs.com/fanzhidongyzby/p/6637084.html" MODIFIED="1498013825788" TEXT="&#x7f51;&#x6587;"/>
+</node>
 </node>
 </map>
