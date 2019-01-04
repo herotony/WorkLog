2ebba09693844e8cad6ce7c0a9dd3a1521295477
herotony <herotony@gmail.com>
2019-01-04 14:26:53 +0800

    init again

---
+++ 2019/job-record/src/main/java/com/saosin/on2019/job/record/month01/TestDate.java
@@ -0,0 +1,12 @@
+package com.saosin.on2019.job.record.month01;
+
+/**
+ * Created by saosin on 2019/1/4.
+ */
+public class TestDate {
+
+    public static void  main(String[] main){
+
+        System.out.println("hello");
+    }
+}
