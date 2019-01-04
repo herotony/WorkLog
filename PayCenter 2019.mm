51565ff5d743fa13b8165de1c87f6d1dbce0ac02
herotony <herotony@gmail.com>
2019-01-04 10:24:19 +0800

    init

--- PayCenter 2019.mm
+++ PayCenter 2019.mm
@@ -1,6 +1,6 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1546423012946" ID="ID_1692601935" MODIFIED="1546423045323">
+<node CREATED="1546423012946" ID="ID_1692601935" MODIFIED="1546423234781">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -12,6 +12,10 @@
   </body>
 </html>
 </richcontent>
-<font NAME="SansSerif" SIZE="18"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1546423185937" ID="ID_1172905900" MODIFIED="1546423231493" POSITION="right" TEXT="&#x6392;&#x67e5;&#x8bb0;&#x5f55;">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1546423247081" ID="ID_1891138099" MODIFIED="1546423254306" TEXT="2019.01.02"/>
+</node>
 </node>
 </map>
